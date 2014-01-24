38293a621751495b07cce4a6fbe66616fcca6acc
Frederic Manisse <fmanisse@gmail.com>
2014-01-24 09:30:05 +0100

    Fix for the tutorial

--- tutorials/t05_enum_example/main.cpp
+++ tutorials/t05_enum_example/main.cpp
@@ -28,14 +28,14 @@ class user
 {
 public:
 	
-	gender	        _gender; // a true enum
-	unsigned int	_type;   // 0 for student, 1 for professor, 2 for visitor
-	unsigned int	_age;
-	std::string	_name;
+    gender          _gender; // a true enum
+    unsigned int    _type;   // 0 for student, 1 for professor, 2 for visitor
+    unsigned int    _age;
+    std::string	    _name;
 
 };
 
-string_array genders = "undefined male female" ;
+string_array genders    = "undefined male female" ;
 string_array user_types = "student professor visitor" ;
 
 void buildClassDescriptor_User()
@@ -43,33 +43,34 @@ void buildClassDescriptor_User()
     auto denum = enum_type_descriptor<gender>::build("gender", genders);
 
     // create class descriptor for the class 'user'
-	auto d = standard_class_descriptor<user>::build("user");
-	d->addField(&user::_gender,"gender");
+    auto d = standard_class_descriptor<user>::build("user");
+    d->addField(&user::_gender,"gender");
     d->addField(&user::_type,"type").annotate(new format_enum("format.all",user_types));
-	d->addField(&user::_age,"age");
-	d->addField(&user::_name,"name");
+    d->addField(&user::_age,"age");
+    d->addField(&user::_name,"name");
 }
 
 void testUser ()
 {
 
-	// let's create our user
-	user Paulo;
-	Paulo._age = 32;
-	Paulo._gender = MALE; // male
-	Paulo._type = 1; // professor
-	Paulo._name = "Paulo" ;
+    // let's create our user
+    user Paulo;
+    Paulo._age = 32;
+    Paulo._gender = MALE; // male
+    Paulo._type = 1; // professor
+    Paulo._name = "Paulo" ;
 
-	// let's serialize Paulo !!
-	std::string destination = json::json::serialize(Paulo);
+    // let's serialize Paulo !!
+    std::string destination = json::json::serialize(Paulo);
 
-	// Let's display my jsoned Paulo :
-	GLOG << "Details of Paulo are :" << std::endl << destination << std::endl;
+    // Let's display my jsoned Paulo :
+    GLOG << "Details of Paulo are :" << std::endl << destination << std::endl;
 
-	// let's clone Paulo with marshalling
-	user PauloCloned;
-	json::json::parse(destination,PauloCloned);
-	GLOG << "Paulo's clone details are : name=" << PauloCloned._name 
+    // let's clone Paulo with marshalling
+    user PauloCloned;
+    json::json::parse(destination,PauloCloned);
+
+    GLOG << "Paulo's clone details are : name=" << PauloCloned._name 
          << ", gender=" << (genders [PauloCloned._gender]) 
          << ", type=" << (user_types[PauloCloned._type]) 
          << ", age=" << ((int) PauloCloned._age) 
